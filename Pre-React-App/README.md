e29bef84e27e9b2ac475be8c8d7d5266394e1b30
Ethan Smith <edhsmith@icloud.com>
2018-03-28 12:16:54 +1300

    Changing code to suit create-react-app and this is the first dev push

---
+++ Pre-React-App/README.md
@@ -0,0 +1,1 @@
+# ethanhsmith.github.io
--- Pre-React-App/client/index.js
+++ Pre-React-App/client/index.js
@@ -17,6 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     <Provider store={store}>
       <App />
     </Provider>,
-    document.getElementById('root')
+    document.getElementById('app')
   )
 })
